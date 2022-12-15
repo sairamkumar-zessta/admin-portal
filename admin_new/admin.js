fa7145cb6f1eff1314852efe3827a4aaf662d7bb
admin <sairamkumar.alladi@zessta.com>
2022-12-15 17:39:34 +0530

    learner trainer tabs added

--- admin_new/admin.js
+++ admin_new/admin.js
@@ -3,33 +3,44 @@ let courses = document.getElementById('course-nav');
 let learner = document.getElementById('learner-nav');
 let addBtn = document.getElementById('add-btn');
 let courseDetails = document.getElementById('courseDetails');
-
+let description = document.getElementById('description');
+let req = document.getElementById('req');
+let courseCard = document.getElementById('courseCard');
+let trainerDetails = document.getElementById('trainerDetails');
+let instructorsFilter = document.getElementById('instructors_filter');
+let learnerDetails = document.getElementById('learnerDetails');
 courses.onclick = function() {
+    instructorsFilter.classList.add('stop-visible');
     addBtn.textContent = "Add Course";
+    description.textContent = "You can manage Course details here.";
     instructor.classList.remove('active');
     courses.classList.add('active');
     learner.classList.remove('active');
     courseDetails.classList.remove('stop-visible');
-    // instructorTable.classList.add('stop-visible');
-    // learnerTable.classList.add('stop-visible');
+    trainerDetails.classList.add('stop-visible');
+    learnerDetails.classList.add('stop-visible');
 }
 instructor.onclick = function() {
+    instructorsFilter.classList.remove('stop-visible');
     addBtn.textContent = "Add Trainer";
+    description.textContent = "You can manage Trainer details here.";
     instructor.classList.add('active');
     courses.classList.remove('active');
     learner.classList.remove('active');
     courseDetails.classList.add('stop-visible');
-    // instructorTable.classList.remove('stop-visible');
-    // learnerTable.classList.add('stop-visible');
+    trainerDetails.classList.remove('stop-visible');
+    learnerDetails.classList.add('stop-visible');
 }
 learner.onclick = function() {
-    addBtn.textContent = "Add Learner";
+    instructorsFilter.classList.add('stop-visible');
+    addBtn.textContent = "Add Learners";
+    description.textContent = "You can manage Learner details here.";
     instructor.classList.remove('active');
     courses.classList.remove('active');
     learner.classList.add('active');
     courseDetails.classList.add('stop-visible');
-    // instructorTable.classList.add('stop-visible');
-    // learnerTable.classList.remove('stop-visible');
+    trainerDetails.classList.add('stop-visible');
+    learnerDetails.classList.remove('stop-visible');
 }
 // addBtn.onclick = function() {
 //     if (addBtn.textContent==="Add Course" || addBtn.textContent==="Add Instructor"){
