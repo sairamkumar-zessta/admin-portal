f46ec2e3162e0ab1c3339bb9d5621b75a88c2e1e
admin <sairamkumar.alladi@zessta.com>
2022-12-20 13:07:25 +0530

    popups added

--- admin_new/admin.js
+++ admin_new/admin.js
@@ -9,6 +9,119 @@ let courseCard = document.getElementById('courseCard');
 let trainerDetails = document.getElementById('trainerDetails');
 let instructorsFilter = document.getElementById('instructors_filter');
 let learnerDetails = document.getElementById('learnerDetails');
+let deleteIcon = document.getElementById('delete-icon');
+let deleteCourseModal = document.getElementById('deleteCourseModal');
+let deleteCancel = document.getElementById('deleteCancel');
+let addLearnerModal = document.getElementById('addLearnerModal');
+let addCourseCancel = document.getElementById('addCourseCancel');
+let courseCardCont = document.getElementById('courseCardCont');
+let trainerEyeIcon = document.getElementById('trainer-eye-icon');
+let deleteTrainerModal = document.getElementById('deleteTrainerModal');
+let deleteTrainerCancel = document.getElementById('deleteTrainerCancel');
+let addLearnerCancel = document.getElementById('addLearnerCancel');
+let singleUserBtn = document.getElementById('singleUser');
+let multipleUserBtn = document.getElementById('multipleUser');
+let singleLearner = document.getElementById('singleLearner');
+let multipleLearners = document.getElementById('multipleLearners');
+let modelTitle = document.getElementById('modelTitle');
+let courses_ =[
+    {
+        id:1,
+        courseName :'Data Science - Python and ML Basics',
+        learnerCount : '123',
+        trainer:'Sidhartha Chinthapally'
+    },
+    {
+        id:2,
+        courseName :'Python',
+        learnerCount : '13',
+        trainer:'Sravanthi Peetla'
+    },
+    {
+        id:3,
+        courseName :'Statistics',
+        learnerCount : '12',
+        trainer:'Sidhartha Chinthapally'
+    },
+    {
+        id:4,
+        courseName :'Data Science - Python and ML Basics',
+        learnerCount : '103',
+        trainer:'Sidhartha Chinthapally'
+    },
+    {
+        id:5,
+        courseName :'python',
+        learnerCount : '11',
+        trainer:'Sidhartha Chinthapally'
+    },
+    {
+        id:6,
+        courseName :'Data Science - Python and ML Basics',
+        learnerCount : '93',
+        trainer:'Sidhartha Chinthapally'
+    },
+]
+
+
+function addAndAppendCourse(course) {
+    let courseCardId = 'courseCard' + course.id;
+    let courseCont = document.createElement('div');
+    courseCont.classList.add('courseCard');
+    courseCont.id = courseCardId;
+    courseCardCont.appendChild(courseCont);
+
+    let courseLeft = document.createElement('div');
+    courseLeft.classList.add('course-card-left');
+    courseCont.appendChild(courseLeft);
+
+    let courseTitle = document.createElement('p');
+    courseTitle.classList.add('course-title');
+    courseLeft.appendChild(courseTitle);
+    courseTitle.textContent = course.courseName;
+
+    let arrangeRow = document.createElement('div');
+    arrangeRow.classList.add('arrange-row');
+    courseLeft.appendChild(arrangeRow);
+
+    let arrangeCol = document.createElement('div');
+    arrangeCol.classList.add('arrange-col');
+    arrangeRow.appendChild(arrangeCol);
+
+    let countTitle = document.createElement('span');
+    countTitle.classList.add('learner-count-title');
+    countTitle.textContent = 'Learner count';
+    arrangeCol.appendChild(countTitle);
+    let count = document.createElement('span');
+    arrangeCol.appendChild(count);
+    count.textContent = course.learnerCount;
+    count.classList.add('learner-count');
+
+    let arrangeColTrain = document.createElement('div');
+    arrangeColTrain.classList.add('arrange-col');
+    arrangeRow.appendChild(arrangeColTrain);
+
+    let trainTitle = document.createElement('span');
+    trainTitle.classList.add('learner-count-title');
+    trainTitle.textContent = 'Trainer name';
+    arrangeColTrain.appendChild(trainTitle);
+    let trainName = document.createElement('span');
+    arrangeColTrain.appendChild(trainName);
+    trainName.textContent = course.trainer;
+    trainName.classList.add('learner-count');
+
+    let deleteIconCont = document.createElement('div');
+    deleteIconCont.classList.add('delete-icon');
+    let deleteIconI = document.createElement('img');
+    deleteIconI.src = 'https://i.postimg.cc/prKj8HZB/delete.png';
+    deleteIconCont.appendChild(deleteIconI);
+    courseCont.appendChild(deleteIconCont);
+
+}
+for (let course of courses_) {
+    addAndAppendCourse(course);
+}
+
 courses.onclick = function() {
     instructorsFilter.classList.add('stop-visible');
     addBtn.textContent = "Add Course";
@@ -42,17 +155,42 @@ learner.onclick = function() {
     trainerDetails.classList.add('stop-visible');
     learnerDetails.classList.remove('stop-visible');
 }
-// addBtn.onclick = function() {
-//     if (addBtn.textContent==="Add Course" || addBtn.textContent==="Add Instructor"){
-//     addCourseModal.classList.remove('stop-visible');
-//     }
-//     else if (addBtn.textContent==="Add Learner"){
-//         addStudentModal.classList.remove('stop-visible');
-//     }
-// }
-// courseClose.onclick = function() {
-//     addCourseModal.classList.add('stop-visible');
-// }
-// studentClose.onclick = function(){
-//     addStudentModal.classList.add('stop-visible');
-// }
+deleteIcon.onclick=function(){
+    deleteCourseModal.style.display='block';
+}
+deleteCancel.onclick=function(){
+    deleteCourseModal.style.display = 'none';
+}
+addCourseCancel.onclick=function(){
+    addCourseModal.style.display = 'none';
+}
+addBtn.onclick = function() {
+    if (addBtn.textContent==="Add Course" || addBtn.textContent==="Add Trainer"){
+    addCourseModal.style.display='block';
+    modelTitle.textContent = addBtn.textContent;
+    }
+    else if (addBtn.textContent==="Add Learners"){
+        addLearnerModal.style.display='block';
+    }
+}
+trainerEyeIcon.onclick = function(){
+    deleteTrainerModal.style.display ='block';
+}
+deleteTrainerCancel.onclick = function(){
+    deleteTrainerModal.style.display ='none';
+}
+addLearnerCancel.onclick = function(){
+    addLearnerModal.style.display = 'none';
+}
+singleUserBtn.onclick=function(){
+    singleUserBtn.classList.add('learner-active');
+    multipleUserBtn.classList.remove('learner-active');
+    singleLearner.style.display='block';
+    multipleLearners.style.display='none';
+}
+multipleUserBtn.onclick=function(){
+    singleUserBtn.classList.remove('learner-active');
+    multipleUserBtn.classList.add('learner-active');
+    singleLearner.style.display='none';
+    multipleLearners.style.display='block';
+}
